71e71db2c75d50176e3c49bb90f1b46652fc3bab
Joel Hounmenou <jesugojoelhounmenou@gmail.com>
2026-05-14 16:57:37 +0100

    Fix package.json

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,18 +1,7 @@
 // This file ensures Prisma knows exactly where to look for your schema and DB URL
-import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import 'dotenv/config';
 
-export default defineConfig({
-  // Chemin vers ton schéma (Module 1 : Inventaire)
-  schema: "prisma/schema.prisma",
-  
-  // Localisation des migrations (Historique de structure)
-  migrations: {
-    path: "prisma/migrations",
-  },
-  
-  // Utilisation de la variable d'environnement définie dans ton .env
-  datasource: {
-    url: process.env["DATABASE_URL"],
-  },
-});
+export default {
+  schema: './prisma/schema.prisma',
+  migrationsPath: './prisma/migrations'
+};
